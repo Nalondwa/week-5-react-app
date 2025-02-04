3f8a99826fe1d8f11975971e615c258f97b63da2
Nalondwa <136497901+Nalondwa@users.noreply.github.com>
2025-02-04 19:01:02 +0200

    Updated Weather.js

--- src/App.js
+++ src/App.js
@@ -6,26 +6,9 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather defaultCity="New York" />
-
-        <footer>
-          This project was coded by Iza Nalondwa
-          <a
-            href="https://github.com/Nalondwa/week-5-react-app"
-            target="_blank"
-            
-          >
-                  open-sourced on GitHub
-                  </a>{" "}
-          and{" "}
-          <a
-            href="https://app.netlify.com/sites/gentle-torte-87304a/overview"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            hosted on Netlify
-            </a>
-        </footer>
+        <Weather defaultCity="Lusaka" />
+       
+        <footer>This project was coded by Iza Nalondwa and is open-sourced on <a href="https://github.com/Nalondwa/week-5-react-app" target="_blank" rel="noreferrer">GitHub</a></footer>
       </div>
       </div>
   );
--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import WeatherInfo from "./WeatherInfo";
-import WeatherForecast from "./WeatherForecast";
 import axios from "axios";
 import "./Weather.css";
 
@@ -32,8 +31,8 @@ export default function Weather(props) {
   }
 
   function search() {
-    const apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    const apiKey = "280309f94e8fdb7oa392f367tcc2b1f8";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query={query}&key={key}`;
     axios.get(apiUrl).then(handleResponse);
   }
 
@@ -61,7 +60,7 @@ export default function Weather(props) {
           </div>
         </form>
         <WeatherInfo data={weatherData} />
-        <WeatherForecast coordinates={weatherData.coordinates} />
+        
       </div>
     );
   } else {

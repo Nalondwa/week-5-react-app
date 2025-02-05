2d142043e332a5637e9d8281ba57862c0e3e5b77
Nalondwa <136497901+Nalondwa@users.noreply.github.com>
2025-02-05 17:42:39 +0200

    updated Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -6,18 +6,18 @@ import "./Weather.css";
 export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
   const [city, setCity] = useState(props.defaultCity);
-
   function handleResponse(response) {
+    console.log(response.data);
     setWeatherData({
       ready: true,
-      coordinates: response.data.coord,
-      temperature: response.data.main.temp,
-      humidity: response.data.main.humidity,
-      date: new Date(response.data.dt * 1000),
-      description: response.data.weather[0].description,
-      icon: response.data.weather[0].icon,
+      city: response.data.city,
+      country: response.data.country,
+      temperature: response.data.temperature.current,
+      description: response.data.condition.description,
+      icon: response.data.condition.icon,
+      humidity: response.data.temperature.humidity,
       wind: response.data.wind.speed,
-      city: response.data.name,
+      date: new Date(response.data.time * 1000),
     });
   }
 
@@ -25,47 +25,33 @@ export default function Weather(props) {
     event.preventDefault();
     search();
   }
-
-  function handleCityChange(event) {
+  function handleUpdateCity(event) {
     setCity(event.target.value);
   }
 
   function search() {
-    const apiKey = "280309f94e8fdb7oa392f367tcc2b1f8"; 
+    let apiKey = "441fcacb9dt020b37114da0ba3e3f5of";
     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
     axios.get(apiUrl).then(handleResponse);
-}
-
+  }
 
   if (weatherData.ready) {
     return (
       <div className="Weather">
         <form onSubmit={handleSubmit}>
-          <div className="row">
-            <div className="col-9">
-              <input
-                type="search"
-                placeholder="Enter a city.."
-                className="form-control"
-                autoFocus="on"
-                onChange={handleCityChange}
-              />
-            </div>
-            <div className="col-3">
-              <input
-                type="submit"
-                value="Search"
-                className="btn btn-primary w-100"
-              />
-            </div>
-          </div>
+          <input
+            type="search"
+            onChange={handleUpdateCity}
+            placeholder="Enter a city"
+          />
+          <input type="submit" value="Search" />
         </form>
         <WeatherInfo data={weatherData} />
-        
+      
       </div>
     );
   } else {
     search();
-    return "Loading...";
+    return null;
   }
 }

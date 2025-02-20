e8ce08044a5ece5e4a38a815785743eff7383942
Nalondwa <136497901+Nalondwa@users.noreply.github.com>
2025-02-20 11:05:54 +0200

    removed snapshot folder

--- forecast-1-day/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders GitHub link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/open-sourced on GitHub/i);
-  expect(linkElement).toBeInTheDocument();
-});

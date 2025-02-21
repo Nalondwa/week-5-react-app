4d613d36b982521793dd5adf34c719f351443d9d
Nalondwa <136497901+Nalondwa@users.noreply.github.com>
2025-02-21 13:17:27 +0200

    update App.css

--- src/App.js
+++ src/App.js
@@ -8,7 +8,29 @@ export default function App() {
       <div className="container">
         <Weather defaultCity="Lusaka" />
        
-        <footer>This project was coded by Iza Nalondwa and is open-sourced on <a href="https://github.com/Nalondwa/week-5-react-app" target="_blank" rel="noreferrer">GitHub</a></footer>
+        <footer>This project was coded by Iza Nalondwa
+          
+        , is{" "}
+          <a
+            href="https://github.com/Nalondwa/week-5-react-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://gentle-torte-87304a.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            hosted on Netlify
+          </a>  
+          
+          
+        
+          </footer>
       </div>
       </div>
   );
